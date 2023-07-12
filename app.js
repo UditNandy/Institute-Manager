ea22f6375e55053b44f621b49f87686139faf312
UditNandy <uditkumarnandy312@gmail.com>
2023-07-12 21:51:05 +0530

    added institutemanager path in main endpoint

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const adminRouter = require("./routes/admin");
 const app = express();
 app.use(express.json());
 
-app.use("/v1/admin", adminRouter);
+app.use("/instituteManager/v1/admin", adminRouter);
 
 module.exports = app;
