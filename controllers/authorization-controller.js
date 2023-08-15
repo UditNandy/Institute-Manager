8ba9abf048db828a5c7ff8b1025d9dec6e1b62d3
UditNandy <uditkumarnandy312@gmail.com>
2023-08-15 22:25:57 +0530

    Merging authorization-management to master

--- controllers/authorization-controller.js
+++ controllers/authorization-controller.js
@@ -1,4 +1,5 @@
 const AuthorizationProfile = require("../models/authorization-model");
+const Admin = require("../models/admin-model");
 
 exports.createAuthorizationProfile = async (req, res) => {
   try {
@@ -15,5 +16,79 @@ exports.fetchAuthorizationProfiles = async (req, res) => {
     res.status(200).json({ status: "Success", data: authorizationProfiles });
   } catch (err) {
     res.status(400).json({ status: "Failed", message: err });
+  }
+};
+
+exports.deleteAuthorizationProfile = async (req, res) => {
+  try {
+    if (
+      !(await Admin.findOne({ authorizationProfile: req.body.profileName }))
+    ) {
+      try {
+        await AuthorizationProfile.deleteMany({
+          profileName: req.body.profileName,
+        });
+        res.status(200).json({
+          status: "Success",
+          message: `${req.body.profileName} deleted successfully`,
+        });
+      } catch (authorizationErr) {
+        res
+          .status(500)
+          .json({ status: "Failed", message: "Something went wrong" });
+      }
+    } else {
+      res.status(400).json({
+        status: "Failed",
+        message: `${req.body.profileName} is already associated with some users`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ status: "Failed", message: "Something went wrong" });
+  }
+};
+
+exports.updateAuthorizationProfile = async (req, res) => {
+  try {
+    if (!(await Admin.findOne({ profileName: req.body.profileName }))) {
+      try {
+        let updatedAuthorizationProfile;
+        if (req.body.newProfileName) {
+          updatedAuthorizationProfile = await AuthorizationProfile.updateOne(
+            {
+              profileName: req.body.profileName,
+            },
+            {
+              profileName: req.body.newProfileName,
+              profiles: req.body.profiles,
+            }
+          );
+        } else {
+          updatedAuthorizationProfile = await AuthorizationProfile.updateOne(
+            {
+              profileName: req.body.profileName,
+            },
+            {
+              profiles: req.body.profiles,
+            }
+          );
+        }
+        res.status(200).json({
+          status: "Success",
+          message: "Authorization profile updated",
+        });
+      } catch (authorizationErr) {
+        res
+          .status(500)
+          .json({ status: "Failed", message: "Something went wrong" });
+      }
+    } else {
+      res.status(400).json({
+        status: "Failed",
+        message: `${req.body.profileName} cant be modified as it is associated with some user`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ status: "Failed", message: "Something went wrong" });
   }
 };
--- routes/authorization-routes.js
+++ routes/authorization-routes.js
@@ -6,9 +6,21 @@ const authorizationRouter = express.Router();
 
 authorizationRouter
   .route("/")
+  .get(
+    authController.verifyToken,
+    authorizationController.fetchAuthorizationProfiles
+  )
   .post(
     authController.verifyToken,
     authorizationController.createAuthorizationProfile
+  )
+  .put(
+    authController.verifyToken,
+    authorizationController.updateAuthorizationProfile
+  )
+  .delete(
+    authController.verifyToken,
+    authorizationController.deleteAuthorizationProfile
   );
 
 module.exports = authorizationRouter;
